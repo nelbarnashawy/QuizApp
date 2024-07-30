8f54009b7ba66a1c7af66a63254a9bfba7074692
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-30 08:52:14 +0300

    Added two new attributes: quizzesTaken - An attribute used to track how many quizzes the user took in his lifetime. quizzesScores - A Hash Map used to map the user's scores of the different "quizzes Taken"

--- src/User.java
+++ src/User.java
@@ -1,8 +1,13 @@
+import java.util.HashMap;
+
 public class User {
     private String name;
+    private int quizzesTaken;
+    private HashMap<Integer, Integer> quizzesScores;
 
     public User(String name) {
         this.name = name;
+        quizzesScores = new HashMap<Integer, Integer>();
     }
     public String getName() {
         return name;
@@ -11,4 +16,11 @@ public class User {
         this.name = name;
     }
 
+    public void addQuizTaken(){
+        quizzesTaken++;
+    }
+
+    public void saveScore(int score) {
+        quizzesScores.put(quizzesTaken, score);
+    }
 }
