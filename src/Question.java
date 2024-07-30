ad8be70bda799047eed1f8a4706456da2f7468d6
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-30 07:31:33 +0300

    Established Question.java with its attributes, constructor, and getters/setters.

--- src/Question.java
+++ src/Question.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Question {
+    private String questionText;
+    private ArrayList<String> possibleAnswers;
+    private int correctAnswer;
+
+    public Question(String questionText, ArrayList<String> possibleAnswers, int correctAnswer) {
+        this.questionText = questionText;
+        this.possibleAnswers = possibleAnswers;
+        this.correctAnswer = correctAnswer;
+    }
+    public String getQuestionText() {
+        return questionText;
+    }
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+    public ArrayList<String> getPossibleAnswers() {
+        return possibleAnswers;
+    }
+    public void setPossibleAnswers(ArrayList<String> possibleAnswers) {
+        this.possibleAnswers = possibleAnswers;
+    }
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+    public void setCorrectAnswer(int correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
 }
