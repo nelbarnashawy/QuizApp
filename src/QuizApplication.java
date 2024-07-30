54212554a6ebb86b2d270e5bf795924a846c4304
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-30 07:48:50 +0300

    Added Quiz quiz to attribute and constructor

--- src/QuizApplication.java
+++ src/QuizApplication.java
@@ -2,9 +2,12 @@ import java.util.ArrayList;
 
 public class QuizApplication {
     private ArrayList<User> users = new ArrayList<User>();
+    private Quiz quiz;
 
     public void registerUser(String name) {
         users.add(new User(name));
+        quiz = new Quiz();
     }
+
 
 }
