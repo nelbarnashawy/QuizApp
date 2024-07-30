ffbd2df89fcf6a44de5b71ede7abf29261b9846e
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-30 07:47:09 +0300

    Established User Registration

--- src/QuizApplication.java
+++ src/QuizApplication.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+
 public class QuizApplication {
+    private ArrayList<User> users = new ArrayList<User>();
+
+    public void registerUser(String name) {
+        users.add(new User(name));
+    }
 
 }
